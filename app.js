3334b5c66ce4afcdfc0971cae6798d6e6455d3f6
hintswen <hintswen@users.noreply.github.com>
2018-10-09 21:59:36 +1100

    Initial commit
    Code taken from: https://www.youtube.com/watch?v=zp4BMR88260

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var words = fs.readFileSync("words_alpha.txt").toString()
+words = words.split("\n");
+
+var badLetters = /[gkmqvwxzio]/;
+var longestAcceptableWord = "";
+
+for( var testWord of words) {
+    if(testWord.length <= longestAcceptableWord.length) {
+        continue;
+    }
+
+    if(testWord.match(badLetters)) { continue; }
+
+    longestAcceptableWord = testWord
+}
+
+console.log(longestAcceptableWord);
